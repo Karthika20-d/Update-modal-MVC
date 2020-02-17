b4a8300805623fc0eacdb7795120ffe93c355c0a
Karthika D <karthikacseksrct@gmail.com>
2020-02-17 21:32:33 +0530

    Add project files.

---
+++ OnlineRealEstate/Controllers/LandController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineRealEstateRepositary;
+using OnlineRealEstateEntity;
+
+namespace OnlineRealEstate.Controllers
+{
+    public class LandController : Controller
+    {
+        public ActionResult Index(Land land)
+        {
+            return RedirectToAction("DisplayLandDetails");
+        }
+        // GET: Land
+        public ActionResult DisplayLandDetails()
+        {
+            IEnumerable<Land> land = LandRepositary.GetLandDetails();
+            ViewBag.Land = land;
+            ViewData["land"] = land;
+            TempData["land"] = land;
+            return RedirectToAction("DisplayDetails");
+        }
+        public ActionResult DisplayDetails()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create_Post(FormCollection formCollection)
+        {
+            Land land = new Land();
+            UpdateModel(land);
+            LandRepositary.Add(land);   
+            return RedirectToAction("DisplayLandDetails");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Land land = LandRepositary.GetLandById(id);
+            return View(land);
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            LandRepositary.Delete(id);
+            return RedirectToAction("DisplayLandDetails");
+        }
+        [HttpPost]
+        public ActionResult Update()
+        {
+            Land land = new Land();
+            TryUpdateModel(land);
+            LandRepositary.Update(land);
+            return RedirectToAction("DisplayLandDetails");
+
+        }
+         
+    }
+}
---
+++ OnlineRealEstateEntity/Land.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineRealEstateEntity
+{
+    public class Land
+    {
+        [Required]
+        public int landId { get; set; }
+        [Required]
+        public string propertyType { get; set; }
+        [Required]
+        public int area { get; set; }
+        [Required]
+        public string location { get; set; }
+
+    }
+}
